09c2ba7aeaaac47f15f2890cad66cb88607e469f
Supratim Mandal <supratimm98@gmail.com>
2026-08-14 16:57:47 +0530

    client api.js url changed

--- server/app.js
+++ server/app.js
@@ -12,7 +12,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: "https://railtracker-eight.vercel.app",
     credentials: true,
   })
 );
