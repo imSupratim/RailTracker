54038c6d1f20b482b305507cce2c8a677279703c
Supratim Mandal <supratimm98@gmail.com>
2026-08-14 16:54:29 +0530

    / get route

--- server/server.js
+++ server/server.js
@@ -8,6 +8,9 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
+app.get("/", (req, res)=>{
+  res.send("Railtracker server is working..✅")
+})
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
